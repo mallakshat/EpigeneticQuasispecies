291c91cc6148a09e2c21248a6cbc5fd80e235ee2
mallakshat <70936499+mallakshat@users.noreply.github.com>
2025-10-09 16:32:51 -0700

    Create FigS10.R

---
+++ Supplementary_Code/FigS10.R
@@ -0,0 +1,11 @@
+data <- read.csv("Csv files for figures/EpigeneticRates_Diagonal.csv")
+
+tiff("Supplementary/Fig 4/EpigeneticRates_Fitness.tiff", width = 6, height = 6, units = "in", res = 300)
+ggplot(data, aes(x = log10(RateOn), y = mean_fit)) +
+  geom_errorbar(aes(ymin = lower_fit, ymax = upper_fit), width = 0.1,linewidth=1) +
+  geom_point(size=4,color="black") + geom_point(size=2.5,color="white")+
+  geom_hline(yintercept = 1.89, linetype = "dashed", color = "red",linewidth=1.25) +
+  theme_classic() + xlab("Rate On")+ylab("Mean Fitness")+
+  scale_x_continuous(breaks = c(0,-1,-2,-3,-4,-5),labels = c(expression(1),expression(10^-1),expression(10^-2),expression(10^-3),expression(10^-4),expression(10^-5)))+
+  theme(text = element_text(size = 26))
+dev.off()
